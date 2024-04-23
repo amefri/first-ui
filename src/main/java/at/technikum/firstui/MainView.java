374d69f1c9dff6c9422d337f42a9677a6d52d398
maria.nwk <marianowak896@gmail.com>
2024-04-23 10:59:54 +0200

    23.04.2024 first commit MN
    (cherry picked from commit 8ba2aeb8dd0d9c3e2f746a0027152c10a4aebeb4)

---
+++ src/main/java/at/technikum/firstui/MainView.java
@@ -0,0 +1,24 @@
+package at.technikum.firstui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainView {
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TableColumn<?, ?> col1, col2, col3, col4, col5;
+
+    @FXML
+    public void initialize() {
+        double numColumns = 5.0;  // Total number of columns
+        col1.prefWidthProperty().bind(tableView.widthProperty().divide(numColumns));
+        col2.prefWidthProperty().bind(tableView.widthProperty().divide(numColumns));
+        col3.prefWidthProperty().bind(tableView.widthProperty().divide(numColumns));
+        col4.prefWidthProperty().bind(tableView.widthProperty().divide(numColumns));
+        col5.prefWidthProperty().bind(tableView.widthProperty().divide(numColumns));
+    }
+}
