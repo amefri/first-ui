5a4aba7c3d8d30a247f7aba37594116a9fbdc2ab
Amelie Frimberger <amelie.frimberger@gmail.com>
2024-04-26 13:51:33 +0200

    menu bar works

--- src/main/java/at/technikum/firstui/view/MenuBarView.java
+++ src/main/java/at/technikum/firstui/view/MenuBarView.java
@@ -1,4 +1,22 @@
 package at.technikum.firstui.view;
 
-public class MenuBarView {
+import at.technikum.firstui.viewmodel.MenuBarViewModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MenuBarView{
+    @FXML
+    private MenuItem menuFileItemClose;
+    @FXML private MenuItem menuEditItemDelete;
+    @FXML private MenuItem menuHelpItemAbout;
+
+    private final MenuBarViewModel viewModel = new MenuBarViewModel();
+
+    @FXML
+    public void initialize() {
+        menuFileItemClose.setOnAction(e -> viewModel.onClose());
+        menuEditItemDelete.setOnAction(e -> viewModel.onDelete());
+        menuHelpItemAbout.setOnAction(e -> viewModel.onAbout());
+    }
 }
--- src/main/java/at/technikum/firstui/viewmodel/MenuBarViewModel.java
+++ src/main/java/at/technikum/firstui/viewmodel/MenuBarViewModel.java
@@ -1,67 +1,78 @@
 package at.technikum.firstui.viewmodel;
 
-public class MenuBarViewModel
+public class MenuBarViewModel{
+    public void onClose() {
+        System.exit(0);
+    }
 
+    public void onDelete() {
+        System.out.println("Delete happens here");
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{
+    public void onAbout() {
+        System.out.println("fancy things that happen in about");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
